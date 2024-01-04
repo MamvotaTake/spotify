135b975c214b5850c2a4ae7275bed602a287271d
Takesure Mamvota <tooyoungmamvota@gmail.com>
2024-01-04 18:12:48 +0700

    Main SideBar links

--- src/layout/navigation/sidebar/Sidebar.tsx
+++ src/layout/navigation/sidebar/Sidebar.tsx
@@ -3,6 +3,8 @@ import Logo from '../../../assets/Logo.png'
 import {PageButton} from "./PageButton.tsx";
 import {GoHome, GoSearch} from "react-icons/go";
 import {LuLibrary} from "react-icons/lu";
+import {FiPlus} from "react-icons/fi";
+import {IoIosArrowForward} from "react-icons/io";
 export function Sidebar() {
     return (
         <div className='Sidebar'>
@@ -16,12 +18,22 @@ export function Sidebar() {
                     icon={<GoSearch/>}
                     title={'Search'}
                 />
-                <PageButton
-                    icon={<LuLibrary/>}
-                    title={'Library'}
-                    // sideIcon={<GoSearch/>}
+                <div className="Sidebar__Link-library">
+                    <PageButton
+                        icon={<LuLibrary/>}
+                        title={'Your Library'}
+                    />
 
-                />
+                    <span>
+                        <PageButton
+                            icon={<FiPlus/>}
+
+                        />
+                         <PageButton
+                             icon={<IoIosArrowForward />}
+                         />
+                    </span>
+                </div>
 
             </ul>
         </div>
