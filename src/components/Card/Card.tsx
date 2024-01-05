c9eed403a03217afba3b529187273138b1234d13
Takesure Mamvota <tooyoungmamvota@gmail.com>
2024-01-05 23:58:13 +0700

    add music card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,25 @@
 import './Card.scss';
-import React from "react";
-import {ButtonBaseProps} from "../Button/Button.tsx";
+import {Button, ButtonBaseProps} from "../Button/Button.tsx";
+import {IoPlay} from "react-icons/io5";
 
 type CardProps = Pick<ButtonBaseProps, 'icon'> &{
-    children?: React.ReactNode;
     className?: string;
     title?: string;
     focused?: boolean;
+    image?: string;
+    type?: string;
 
 
 }
-export function Card({children, className, title, focused=false}: CardProps) {
+export function Card({type, title, focused=false, image}: CardProps) {
     return (
-        <>Card</>
+        <div className='Card'>
+            <img src={image} alt="image" className='Card__Image'/>
+            <p className='Card__Content'>
+                {title}
+                <span>{type}</span>
+            </p>
+            <Button className="Card__Button">{<IoPlay/>}</Button>
+        </div>
     );
 }
