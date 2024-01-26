4f56dfeafe76b518e56ba51ae302f9df9e485dc0
Takesure Mamvota <tooyoungmamvota@gmail.com>
2024-01-26 23:25:02 +0700

    CU-86bx7x59t - Home

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,24 +10,29 @@ type CardProps = Pick<ButtonBaseProps, 'icon'> &{
     image?: string;
     type?: string;
     display?: 'row' | 'column';
+    fullWidth?: boolean;
+    imageSize?: 'small' | 'medium' | 'big'
+    playButtonPosition?: 'top' | 'bottom' | 'left' | 'right';
 
 
 
 }
-export function Card({type, title, className, image, display}: CardProps) {
+export function Card({type, title, className, image, display, fullWidth, imageSize="medium", playButtonPosition='right'}: CardProps) {
     return (
         <div className={clsx(
             'Card',
             `Card--${display}`,
+            `Card--${type}`,
+            fullWidth && 'Card--full-width',
             className,
-            `Card--${type}`
+
         )}>
-            <img src={image} alt="image" className='Card__Image'/>
+            <img src={image} alt="image" className={`Card__Image Card__Image--${imageSize}`}/>
             <p className='Card__Content'>
                 {title}
                 <span>{type}</span>
             </p>
-            <Button className="Card__Button">{<IoPlay/>}</Button>
+            <Button className={`Card__Button Card__Button-${playButtonPosition}`} >{<IoPlay/>}</Button>
         </div>
     );
 }
